f2b859e72c5cf2b9b237dc21f2e6ba26a51fc24a
Theophilos Kantartzis <kantartzistheophilos@gmail.com>
2019-01-10 20:26:35 +0200

    asda

--- js/TEST.js
+++ js/TEST.js
@@ -2,14 +2,18 @@ function isNotEmpty(field) {
 
     var fieldData = field.value;
 
-    if (fieldData.length == 0 || fieldData == "") {
+    if (fieldData.length == 0 || fieldData == "" || fieldData==fieldData) {
 
         field.className = "FieldError"; //Classs to highlight error
-        //document.write("Please correct the errors in order to continue.");
+        allert("Please correct the errors in order to continue.");
         return false;
     } else {
 
         field.className = "FieldOk"; //Resets field back to default
         return true; //Submits form
     }
+}
+
+function return1(void) {
+	document.cookie = "myJavascriptVar = 'Please correct the errors in order to continue.' ";
 }
--- php/Student-SignUp.php
+++ php/Student-SignUp.php
@@ -3,7 +3,7 @@
 	<head>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href='../css/Student-SignUp.css'>
-		<script type="text/javascript" src="../JS/TEST.js"></script>
+		<script type="text/javascript" src="../js/TEST.js"></script>
 	</head>
 
 	<body class="background-color">
@@ -34,7 +34,12 @@
 			<div class="signup-box" ">
 				<div class="form-subbox">
 					<form action = "signup.php" method = "post" onsubmit="return validateForm(this)">  
-                  		<label class="signup-info" style="margin-top: 100px;">ΟΝΟΜΑ ΧΡΗΣΤΗ :</label><input onChange="isNotEmpty(user);" type = "text" name = "user" class = "box"/><br /><br />
+					</form>
+                  		<label class="signup-info" style="margin-top: 100px;">ΟΝΟΜΑ ΧΡΗΣΤΗ :</label><input onChange="return1();" type = "text" name = "user" class = "box"/><br /><br />
+                  		<?php 
+						   $phpVar =  $_COOKIE['myJavascriptVar'];
+						   echo $phpVar;
+						?>
                  		<label class="signup-info" >ΚΩΔΙΚΟΣ ΠΡΟΣΒΑΣΗΣ :</label><input type = "password" name = "pass" class = "box" /><br/><br />
                  		<label class="signup-info" >ΕΠΑΛΗΘΕΥΣΗ ΚΩΔΙΚΟΥ ΠΡΟΣΒΑΣΗΣ :</label><input type = "password" name = "pass_cor" class = "box" /><br/><br />
                   		<input type = "submit" value = "submit" style="float: right;margin-right: 375px;margin-bottom: 150px;" /><br />
