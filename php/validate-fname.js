9c63bbf44dde7fad23c59c29a6eb5b3121af8e36
nikoskaraiskakis <sdi1400062@di.uoa.gr>
2019-01-11 19:47:41 +0200

    x0ax0a

--- php/validate-fname.js
+++ php/validate-fname.js
@@ -3,7 +3,6 @@ function val_fname(input,input_error) {
 	var flag = 0;
 	var re1 = /^[Α-Ωα-ω]+$/;
 	var re2 = /^[A-Za-z]+$/;
-	var symbols = /^[/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]]+$/;
 
 	if ( check_str.length >= 1) {
 		document.getElementById(input_error).innerHTML = "";
@@ -13,12 +12,12 @@ function val_fname(input,input_error) {
 		document.getElementById(input_error).innerHTML = " Παρακαλούμε συμπληρωστε το πεδίο Ονομα";
 	}
 
-	if((re1 && !re2) || (!re1 && re2)){
+	if(re1.test(check_str) && re2.test(check_str) ){
 		document.getElementById(input_error).innerHTML = "Το όνομα μπορεί να περιέχιε μόνο ελληνικούς ή μόνο λατινικούς χαρακτήρες";
 	}
 
 	if (flag) {
-		if (symbols.test(check_str) ) {
+		if (! re.test(check_str) ) {
 			document.getElementById(input_error).innerHTML = "Το όνομα δεν μπορεί να περιέχει αριθμούς ή σύμβολα";
 		}
 		else {
