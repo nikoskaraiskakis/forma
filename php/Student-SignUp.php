fb124e035cb8b0007bbca8f9a23cdd461a827f25
nikoskaraiskakis <sdi1400062@di.uoa.gr>
2019-01-12 22:02:04 +0200

    xd

--- php/Student-SignUp.php
+++ php/Student-SignUp.php
@@ -2,8 +2,14 @@
 <html>
 	<head>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-		<link rel="stylesheet" type="text/css" href='../css/Student-SignUp.css'>
-		<script type="text/javascript" src="validate-fname.js"></script>
+		<link rel="stylesheet" type="text/css" href='../css/Publisher-SignUp.css'>
+		<script type="text/javascript" src="../JAVAscripts/validate-user.js"></script>
+		<script type="text/javascript" src="../JAVAscripts/validate-email.js"></script>
+		<script type="text/javascript" src="../JAVAscripts/validate-cname.js"></script>
+		<script type="text/javascript" src="../JAVAscripts/validate-afm.js"></script>
+	    <script type="text/javascript" src="../JAVAscripts/validate-pass.js"></script>
+	    <script type="text/javascript" src="../JAVAscripts/validate-afm.js"></script>
+	    <script type="text/javascript" src="../JAVAscripts/val-signup.js"></script>
 	</head>
 
 	<body class="background-color">
@@ -32,18 +38,30 @@
 		</section>
 		<section class ="mid-section">
 			<div class="signup-box" ">
-				<div class="form-subbox">
-					<form action = "signup.php" method = "post" onsubmit="return validateForm(this)">  
-					</form>
-                  		<label class="signup-info" style="margin-top: 100px;">ΟΝΟΜΑ :</label><input id="fname_inp" onChange="val_fname('fname_inp','fname_err');" type = "text" name = "user" class = "box"/><br /><br />
+				<form method = "post" onsubmit="return re('uname_inp','pass_inp','cpass_inp','em_inp','cname_inp','afm_inp','alert_box')" action = "publ-signup.php" >  
+					<div class="form-subbox">
+                  		<label class="signup-info" style="margin-top: 60px;"><a style="font-size: 12PT;">ΟΝΟΜΑ ΧΡΗΣΤΗ :</a></label><input style="height: 30px;" id="uname_inp" onChange="val_user('uname_inp','uname_err');" type = "text" name = "user" class = "box"/><br /><br />
 
-                  		<div id="fname_err" style="color: red;"></div>
+                  			<div id="uname_err" style="color: red;font-size: 12pt;"></div>
 
-                 		<label class="signup-info" >ΚΩΔΙΚΟΣ ΠΡΟΣΒΑΣΗΣ :</label><input type = "password" name = "pass" class = "box" /><br/><br />
-                 		<label class="signup-info" >ΕΠΑΛΗΘΕΥΣΗ ΚΩΔΙΚΟΥ ΠΡΟΣΒΑΣΗΣ :</label><input type = "password" name = "pass_cor" class = "box" /><br/><br />
-                  		<input type = "submit" value = "submit" style="float: right;margin-right: 375px;margin-bottom: 150px;" /><br />
-               		</form>
-				</div>
+                 		<label class="signup-info" ><a style="font-size: 12pt;">ΚΩΔΙΚΟΣ ΠΡΟΣΒΑΣΗΣ :</a></label><input style="height: 30px;" id="pass_inp" onChange="val_pass('pass_inp','pass_err');" type = "password" name = "pass" class = "box" /><br/><br />
+
+                 			<div id="pass_err" style="color: red;font-size: 12pt;"></div>
+
+                 		<label class="signup-info" ><a style="font-size: 12pt;">ΕΠΑΛΗΘΕΥΣΗ ΚΩΔΙΚΟΥ ΠΡΟΣΒΑΣΗΣ :</a></label><input style="height: 30px;" id="cpass_inp" type = "password" name = "pass_cor" class = "box" /><br/><br />
+
+                 			<div id="cpass_err" style="color: red;font-size: 12pt;"></div>
+
+                 		<label class="signup-info" ><a style="font-size: 12pt;">EMAIL :</a></label><input style="height: 30px;" type = "email" name = "em" class = "box" id="em_inp" onChange="val_em('em_inp','em_err');"/><br/><br />
+
+                 			<div id="em_err" style="color: red;font-size: 12pt;"></div>
+
+                  	</div>
+                  	<div style=" float: right;width: 30%;height: 600px;">
+                  		<input type = "submit" value = "ΕΓΓΡΑΦΗ" style="margin-top: 250px;font-size: 20pt;" /><br />
+                      <div id="alert_box" style="margin-right: 10px;background-color: white;color: red;font-size: 12pt;"></div>
+                  	</div>
+               	</form>
 			</div>
 		</section>
 	</body>
---
+++ php/stud-signup.php
@@ -0,0 +1,21 @@
+<?php
+    include("connect.php");
+    session_start();
+    if($_SERVER["REQUEST_METHOD"] == "POST") {   
+        $username=$_POST['user'];
+        $password=$_POST['pass'];
+        $password_cor=$_POST['pass_cor'];
+        $email=$_POST['em'];
+        $sql1 = "SELECT * FROM Users WHERE User_Name LIKE '$username'";
+        $result = mysqli_query($db,$sql1);
+        if ( mysqli_num_rows($result) != 0 ) {
+            $_SESSION['error']="To username ειναι ηδη πιασμενο";
+            header("location: error.php");
+        }
+        else {
+            $sql = "INSERT INTO Users(User_Name,Pass,User_Type) VALUES ('$username','$password','STUDENT')";
+            mysqli_query($db,$sql);
+            header("location: index.php");
+        }
+    }
+?>
